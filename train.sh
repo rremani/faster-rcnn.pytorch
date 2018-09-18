874798f626b4f736b4493b353da201d0b2a46464
root <root@hypothizer-gpu-dev-001.c.hypothizerdevelopment.internal>
2018-09-18 07:56:20 +0000

    minor changes

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python trainval_net.py  --dataset table --net res101 --epochs 30 --bs 1 --nw 1  --lr 0.0001 --lr_decay_step 1000   --cuda --o adam
+python trainval_net.py  --dataset table --net res101 --s 1 --epochs 30 --bs 1 --nw 1  --lr 0.0001 --lr_decay_step 1000   --cuda --o adam
